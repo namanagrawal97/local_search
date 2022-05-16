a4ad690ed9103a6cb6ce08fdbb7621cfcc64f530
namanagrawal97 <96447922+namanagrawal97@users.noreply.github.com>
2022-05-16 11:03:54 -0400

    Add files via upload
    Made changes so the FRAvisits table now consists of columns lengths of different lengths.

--- combinedresidenceplot - Copy.py
+++ combinedresidenceplot - Copy.py
@@ -17,15 +17,15 @@ os.chdir('G:\\My Drive\\local_search\\')
 #Set Parameters here. 
 
 food="1M"
-starvation="24" 
+starvation="16" 
 t=3
 trajtime=40    
-fnames = glob.glob('local_search/'+food+'/'+starvation+'/'+'*.csv')
+fnames = glob.glob('local_search_final/'+food+'/'+starvation+'/'+'*.csv')
 print(fnames)
 
 
-data_header = ['Time', 'Latency', 'Fx1', 'Fy1', 'Fx2', 'Fy2', 'Fx3', 'Fy3','Fx4','Fy4']
-data_header2 = ['Fx1', 'Fy1', 'Fx2', 'Fy2', 'Fx3', 'Fy3','Fx4','Fy4']
+data_header = ['Time', 'Latency', 'Fx1', 'Fy1', 'Fx2', 'Fy2', 'Fx3', 'Fy3']
+data_header2 = ['Fx1', 'Fy1', 'Fx2', 'Fy2', 'Fx3', 'Fy3']
 FRAvisits=pd.DataFrame()#Loads the dataframe
 afterfirstvisit=pd.DataFrame(columns = ['Fx', 'Fy','Flynum','Time'])#generate empty dataframe
 beforefirstvisit=pd.DataFrame(columns = ['Fx', 'Fy','Flynum','Time'])#generate empty dataframe
@@ -36,9 +36,10 @@ for u in fnames:#goes thru files in the folder.
     df=pd.read_csv(u, header=None)
     df.columns=data_header#sets the column header
     df['Latency'][0] = 0#sets the first value of latency as zero because it is generally very high
+    df['Time']=df['Time']-60
     for i in range(0,len(data_header2),2):
         empty=pd.DataFrame()
-        empty=df[(df[data_header2[i]]>440) & (df[data_header2[i]] < 640) & (df[data_header2[i+1]] < 640) & (df[data_header2[i+1]] > 440)]
+        empty=df[(df[data_header2[i]]>554) & (df[data_header2[i]] < 654) & (df[data_header2[i+1]] < 590) & (df[data_header2[i+1]] > 490)]
         #here we find when the fly is within 440 and 640 pixels.
         print(data_header2[i],i)
         print(data_header2[i+1], 'summer')
@@ -73,7 +74,10 @@ for u in fnames:#goes thru files in the folder.
         except:
             pass
         try:
-            FRAvisits[k]=pd.Series(jumps)
+            # pd.concat([FRAvisits,jumps], ignore_index=True, axis=1)
+            # FRAvisits[k]=pd.Series(jumps)
+            jumps=pd.Series(jumps)
+            FRAvisits=pd.concat((FRAvisits,jumps.rename(k)), axis=1)
             indexing=np.where(df['Time']>jumps[0])#Finds the first feeding bout
             index=indexing[0][0]
             aftertrunc=df.truncate(before=index)
@@ -111,8 +115,8 @@ ax3=axx.flat[2]
 ax4=axx.flat[3]
 
                        
-h=ax2.hist2d(x=afterfirstvisit['Fx'], y=afterfirstvisit['Fy'], bins=(50, 50), cmap=plt.cm.Greys, density=True)
-h2=ax1.hist2d(x=beforefirstvisit['Fx'], y=beforefirstvisit['Fy'], bins=(50, 50), cmap=plt.cm.Greys, density=True)
+h=ax2.hist2d(x=afterfirstvisit['Fx'], y=afterfirstvisit['Fy'], bins=(50, 50), cmap=plt.cm.plasma, density=True)
+h2=ax1.hist2d(x=beforefirstvisit['Fx'], y=beforefirstvisit['Fy'], bins=(50, 50), cmap=plt.cm.plasma, density=True)
 ax2.set_title("cumulative residence after first eating bout", fontsize=13)
 #plt.colorbar(h, ax=ax2)
 #plt.colorbar(h2, ax=ax1)
@@ -140,22 +144,22 @@ for f in flynum:
                 
 sns.set_style("white")
 #fig7,(ax3,ax4) = plt.subplots(nrows=1,ncols=2,sharey=False, figsize=(15,7))
-scatter=ax4.scatter(data=trajectoryafter, x='Fy', y='Fx', s=1, c='Flynum', cmap='Dark2')
+scatter=ax4.scatter(data=trajectoryafter, x='Fx', y='Fy', s=1, c='Flynum', cmap='hsv')
 legend1 = ax4.legend(*scatter.legend_elements(), loc="upper right", title="flies")
 ax4.add_artist(legend1)
 ax4.set_xlim(0,1080)
 ax4.set_ylim(0,1080)
 ax4.axvline(x=540.0, color="grey")
-ax4.axhline(y=540.0, color="grey")
+ax4.axhline(y=604.0, color="grey")
 ax4.set_title('trajectory {} seconds after first encounter with food'.format(trajtime), fontsize=13)
 
-scatter2=ax3.scatter(data=trajectorybefore, x='Fy', y='Fx', s=1, c='Flynum', cmap='Dark2')
+scatter2=ax3.scatter(data=trajectorybefore, x='Fx', y='Fy', s=1, c='Flynum', cmap='hsv')
 legend2 = ax3.legend(*scatter2.legend_elements(), loc="upper right", title="flies")
 ax3.add_artist(legend2)
 ax3.set_xlim(0,1080)
 ax3.set_ylim(0,1080)
 ax3.axvline(x=540.0, color="grey")
-ax3.axhline(y=540.0, color="grey")
+ax3.axhline(y=604.0, color="grey")
 ax3.set_title('trajectory {} seconds before first encounter with food'.format(trajtime), fontsize=13)
 
 plt.show()
@@ -163,4 +167,20 @@ fig6.suptitle('{} {}h'.format(food, starvation), fontsize=20, y=0.5)
 fig6.savefig('Trajectory{}{}.png'.format(food, starvation),format='png', dpi=300, bbox_inches = 'tight')
 
 
-
+cmap_list=['PiYG', 'PRGn', 'BrBG', 'PuOr', 'RdGy', 'RdBu', 'RdYlBu',
+                      'RdYlGn', 'Spectral', 'coolwarm', 'bwr', 'seismic']
+fig,ax=plt.subplots()
+empty=pd.DataFrame()
+for i in flynum:
+    empty=trajectoryafter[trajectoryafter['Flynum']==i]
+    ax.scatter(data=empty, x='Fy', y='Fx', s=1, c='Time', cmap=cmap_list[i], label=i)
+#ax.legend(*cmap_list.legend_elements(), loc="upper right", title="flies")
+plt.legend()
+ax = plt.gca()
+legend = ax.get_legend()
+# legend.legendHandles[0].set_color(cmap_list[1](0.8))
+# legend.legendHandles[1].set_color(cmap_list[2](0.8))
+# legend.legendHandles[2].set_color(cmap_list[3](0.8))
+fig.savefig('Trajectory2{}{}.png'.format(food, starvation),format='png', dpi=300, bbox_inches = 'tight')
+plt.show()
+    
