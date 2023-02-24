f37b6f323d8db8d76e28e139c433bc24360fb21d
namanagrawal97 <na488@cornell.edu>
2023-02-24 18:46:51 -0500

    Added plotting for individual flies
    Added plotting inst vel before and after eating food for each fly.

--- final_screening_analysis_before_vel.py
+++ final_screening_analysis_before_vel.py
@@ -36,7 +36,7 @@ import os
 import scipy.stats as stats
 import matplotlib.ticker as ticker
 
-os.chdir('C:\\Users\\Yapicilab\\Dropbox\\screening') #SET THIS TO YOUR FOLDER WHERE YOU HAVE KEPT THE DATA FILES
+os.chdir('C:\\Users\\sinha\\Dropbox\\screening') #SET THIS TO YOUR FOLDER WHERE YOU HAVE KEPT THE DATA FILES
 real_food_all=pd.read_csv('food_timing_screening.csv')
 
 
@@ -50,10 +50,10 @@ foodlist.remove('food_timing_screening.csv')
 foodlist.remove('results')
 foodlist.remove('bad_data') #Removes this genotype because there is some problem with this data
 genotypelist=foodlist
-# genotypelist=['ss47292']
+genotypelist=['ss47080']
 #USE THIS LINE OF CODE TO SET YOUR GENOTYPE.
 starvation="24"
-time_list=[60]
+time_thres=60
 
 # time_thres=60
 
@@ -82,7 +82,7 @@ def dict_to_df(input_dict):
 def df_to_csv(input_df):
     input_df.to_csv("results\\{}.csv".format(input_df),index=False)
     
-def custom_plot(input_df):
+def box_plot(input_df):
     fig, ax= plt.subplots()
     sns.set_style("white")
     ax = sns.boxplot(data=input_df, palette="Set3", showfliers = False, showmeans=False, linewidth=1, fliersize=3, orient="h")
@@ -97,156 +97,188 @@ def custom_plot(input_df):
     ax.xaxis.grid(True)
     plt.show()
 
-for time_thres in time_list: 
+def individual_plot(input_df):
+    fig, axs = plt.subplots(nrows=3, ncols=3, figsize=(12, 12))
+    plt.subplots_adjust(hspace=0.5)
+    fig.suptitle(" Instantaneous Velocity ({}s after food) distribution".format(time_thres), fontsize=18, y=0.95)
+
+    for genotype, ax in zip(list(input_df.columns), axs.ravel()):
+        newlist = [x for x in list(input_df[genotype]) if np.isnan(x) == False]
+
+        ax.plot(np.arange(0,len(newlist),1), newlist)
+        # ax.plot(np.arange(0,time_thres*24,1), [60]*time_thres*24) 
+        ax.set_title("{}".format(genotype),fontsize=9)
+        # ax.set_ylim(0,540)
+        # ax.set_xlim(0,len(input_df[genotype]))
+        ax.set_xlabel("")
+        ax.axvline(len(newlist)/2, color='red')
+        # ticks = ax.get_xticks()/25
+        # ax.set_xticklabels(ticks)
+
+
+inst_vel_after_mean_all_dict={}
+inst_vel_before_mean_all_dict={}
+inst_vel_diff_dict={}
+inst_vel_ratio_dict={}
+
+inst_vel_after_binned_mean_all_dict={}
+inst_vel_before_binned_mean_all_dict={}
+inst_vel_diff_binned_dict={}
+inst_vel_ratio_binned_dict={}
+number_of_flies={}    
+
+for genotype in genotypelist:
+                
+    print(genotype,starvation)
+    fnames = sorted(glob.glob(genotype+'/'+starvation+'/'+'*.csv')) #Enter the genotype folder and "24" folder
+    index=0 #Counter for counting the number of iterations
     
-
-    inst_vel_after_mean_all_dict={}
-    inst_vel_before_mean_all_dict={}
-    inst_vel_diff_dict={}
-    inst_vel_ratio_dict={}
+    real_food_df=real_food_all[real_food_all['genotype']==genotype] #Extracts data for this genotype from Manual labels of first food eating labels
+    # real_food_df=real_food_df[real_food_df['starvation']==int(starvation)]
+    real_food_bout_list=list(real_food_df['final_first_bout'])
     
-    inst_vel_after_binned_mean_all_dict={}
-    inst_vel_before_binned_mean_all_dict={}
-    inst_vel_diff_binned_dict={}
-    inst_vel_ratio_binned_dict={}
-    number_of_flies={}    
-    for genotype in genotypelist:
-                    
-        print(genotype,starvation)
-        fnames = sorted(glob.glob(genotype+'/'+starvation+'/'+'*.csv')) #Enter the genotype folder and "24" folder
-        index=0 #Counter for counting the number of iterations
-        
-        real_food_df=real_food_all[real_food_all['genotype']==genotype] #Extracts data for this genotype from Manual labels of first food eating labels
-        # real_food_df=real_food_df[real_food_df['starvation']==int(starvation)]
-        real_food_bout_list=list(real_food_df['final_first_bout'])
-        
-        inst_vel_all=[]
-        
-        inst_vel_after_mean_all=[]
-        inst_vel_before_mean_all=[]
-        inst_vel_diff_all=[]
-        inst_vel_ratio_all=[]        
+    inst_vel_all=[]
+    inst_vel_behav=[]
+    inst_vel_behav_binned=[]
+    inst_vel_behav_dict={}
+    inst_vel_behav_binned_dict={}
+    
+    inst_vel_after_mean_all=[]
+    inst_vel_before_mean_all=[]
+    inst_vel_diff_all=[]
+    inst_vel_ratio_all=[]        
 
-        inst_vel_after_binned_mean_all=[]
-        inst_vel_before_binned_mean_all=[]
-        inst_vel_diff_binned_all=[]        
-        inst_vel_ratio_binned_all=[]
+    inst_vel_after_binned_mean_all=[]
+    inst_vel_before_binned_mean_all=[]
+    inst_vel_diff_binned_all=[]        
+    inst_vel_ratio_binned_all=[]
 
-        for u in fnames: #goes through files in the folder.
-            
-            """
-            Loading the data into a dataframe
-            """
-            
-            df=pd.read_csv(u, header=None)
-            # print("before",df.shape[1])
-            # df=df.dropna(axis=1,thresh=20000)
-            # print("after",df.shape[1])
-            if(df.shape[1]==10):
-                data_header = ['Time', 'Latency', 'Fx1', 'Fy1', 'Fx2', 'Fy2', 'Fx3', 'Fy3','Fx4','Fy4']
-            elif(df.shape[1]==8):
-                data_header = ['Time', 'Latency', 'Fx1', 'Fy1', 'Fx2', 'Fy2', 'Fx3', 'Fy3']
-            elif(df.shape[1]==6):
-                 data_header = ['Time', 'Latency', 'Fx1', 'Fy1', 'Fx2', 'Fy2']
-            elif(df.shape[1]==4):
-                 data_header = ['Time', 'Latency', 'Fx1', 'Fy1']
-            df.columns=data_header
-            
-            df['Time']=df['Time']-round(df['Time'][0]) #Since our experiment begins after 2 minutes, we subtract this time from the data
-            
-            for i in range(2,len(data_header),2):
-                index=index+1
-                first_bout_time=real_food_bout_list[index-1] #Extracting the first bout time from manual annotation and storing it in a variable
-                if (first_bout_time==2000):
-                    continue
+    for u in fnames: #goes through files in the folder.
+        
+        """
+        Loading the data into a dataframe
+        """
+        
+        df=pd.read_csv(u, header=None)
+        # print("before",df.shape[1])
+        # df=df.dropna(axis=1,thresh=20000)
+        # print("after",df.shape[1])
+        if(df.shape[1]==10):
+            data_header = ['Time', 'Latency', 'Fx1', 'Fy1', 'Fx2', 'Fy2', 'Fx3', 'Fy3','Fx4','Fy4']
+        elif(df.shape[1]==8):
+            data_header = ['Time', 'Latency', 'Fx1', 'Fy1', 'Fx2', 'Fy2', 'Fx3', 'Fy3']
+        elif(df.shape[1]==6):
+             data_header = ['Time', 'Latency', 'Fx1', 'Fy1', 'Fx2', 'Fy2']
+        elif(df.shape[1]==4):
+             data_header = ['Time', 'Latency', 'Fx1', 'Fy1']
+        df.columns=data_header
+        
+        df['Time']=df['Time']-round(df['Time'][0]) #Since our experiment begins after 2 minutes, we subtract this time from the data
+        
+        for i in range(2,len(data_header),2):
+            index=index+1
+            first_bout_time=real_food_bout_list[index-1] #Extracting the first bout time from manual annotation and storing it in a variable
+            if (first_bout_time==2000):
+                continue
+            else:
+                pass
+
+                time=list(df['Time'])
+                split_point_index=find_index(time, first_bout_time)
+                print(index,first_bout_time,split_point_index)
+                latency=list(df['Latency'])
+                latency[0]=np.mean(latency)
+                
+                x_coord=list(df[data_header[i]])
+                y_coord=list(df[data_header[i+1]])
+                inst_vel_all=np.zeros_like(x_coord)
+                
+                for j in range(0,len(x_coord)-1,1):
+                    inst_vel_all[j]=np.sqrt((x_coord[j+1]-x_coord[j])**2+(y_coord[j+1]-y_coord[j])**2)/latency[j+1] #Calculate Instantenous Velocity                    
+                
+                
+                inst_vel_before=inst_vel_all[0:split_point_index]
+                inst_vel_after=inst_vel_all[split_point_index:len(inst_vel_all)]
+                
+                if(split_point_index>time_thres*24 and split_point_index<(len(x_coord)-time_thres*24)):
+                    inst_vel_before=inst_vel_before[len(inst_vel_before)-time_thres*24:]
+                    inst_vel_after=inst_vel_after[0:time_thres*24]
                 else:
-                    pass
-    
-                    time=list(df['Time'])
-                    split_point_index=find_index(time, first_bout_time)
-                    print(index,first_bout_time,split_point_index)
-                    latency=list(df['Latency'])
-                    latency[0]=np.mean(latency)
-                    
-                    x_coord=list(df[data_header[i]])
-                    y_coord=list(df[data_header[i+1]])
-                    inst_vel_all=np.zeros_like(x_coord)
-                    
-                    for j in range(0,len(x_coord)-1,1):
-                        inst_vel_all[j]=np.sqrt((x_coord[j+1]-x_coord[j])**2+(y_coord[j+1]-y_coord[j])**2)/latency[j+1] #Calculate Instantenous Velocity                    
-                    
-                    
-                    inst_vel_before=inst_vel_all[0:split_point_index]
-                    inst_vel_after=inst_vel_all[split_point_index:len(inst_vel_all)]
-                    
-                    if(split_point_index>time_thres*24 and split_point_index<(len(x_coord)-time_thres*24)):
-                        inst_vel_before=inst_vel_before[len(inst_vel_before)-time_thres*24:]
-                        inst_vel_after=inst_vel_after[0:time_thres*24]
+                    if (len(inst_vel_before)>len(inst_vel_after)):
+                        inst_vel_before=inst_vel_before[-len(inst_vel_after):]
+                    elif (len(inst_vel_before)<len(inst_vel_after)):
+                        inst_vel_after=inst_vel_after[0:len(inst_vel_before)]
                     else:
-                        if (len(inst_vel_before)>len(inst_vel_after)):
-                            inst_vel_before=inst_vel_before[-len(inst_vel_after):]
-                        elif (len(inst_vel_before)<len(inst_vel_after)):
-                            inst_vel_after=inst_vel_after[0:len(inst_vel_before)]
-                        else:
-                            pass
-                        
-                    inst_vel_after_binned=binning(inst_vel_after,10)
-                    inst_vel_before_binned=binning(inst_vel_before,10)
+                        pass
                     
-                    inst_vel_after_mean_all.append(np.nanmean(inst_vel_after))  #Calculate the mean Instantenous Velocity and append to a list                 
-                    inst_vel_before_mean_all.append(np.nanmean(inst_vel_before))
-                    inst_vel_diff_all.append(np.nanmean(inst_vel_after)-np.nanmean(inst_vel_before))
-                    inst_vel_ratio_all.append(np.nanmean(inst_vel_after)/np.nanmean(inst_vel_before))
-                    
-                    inst_vel_after_binned_mean_all.append(np.nanmean(inst_vel_after_binned))  #Calculate the mean Instantenous Velocity and append to a list                 
-                    inst_vel_before_binned_mean_all.append(np.nanmean(inst_vel_before_binned))
-                    inst_vel_diff_binned_all.append(np.nanmean(inst_vel_after_binned)-np.nanmean(inst_vel_before_binned))
-                    inst_vel_ratio_binned_all.append(np.nanmean(inst_vel_after_binned)/np.nanmean(inst_vel_before_binned))
-                    
-        
-        number_of_flies[genotype]=index
-        
-        
-        inst_vel_after_mean_all_dict[genotype]=inst_vel_after_mean_all #Append the Mean Instantaneous Velocity LIST to Dictionary with the Corresponding Genotype
-        inst_vel_before_mean_all_dict[genotype]=inst_vel_before_mean_all
-        inst_vel_diff_dict[genotype]=inst_vel_diff_all
-        inst_vel_ratio_dict[genotype]=inst_vel_ratio_all
-        
-        inst_vel_after_binned_mean_all_dict[genotype]=inst_vel_after_binned_mean_all #Append the Mean Instantaneous Velocity LIST to Dictionary with the Corresponding Genotype
-        inst_vel_before_binned_mean_all_dict[genotype]=inst_vel_before_binned_mean_all
-        inst_vel_diff_binned_dict[genotype]=inst_vel_diff_binned_all
-        inst_vel_ratio_binned_dict[genotype]=inst_vel_ratio_binned_all
-        
-    """
-    Converting Dictionaries to Dataframe using functions
-    
-    """
-    inst_vel_after_mean_all_df = dict_to_df(inst_vel_after_mean_all_dict)
-    inst_vel_after_binned_mean_all_df= dict_to_df(inst_vel_after_binned_mean_all_dict)
-    inst_vel_before_mean_all_df = dict_to_df(inst_vel_before_mean_all_dict)
-    inst_vel_before_binned_mean_all_df = dict_to_df(inst_vel_before_binned_mean_all_dict)
-    inst_vel_diff_all_df=dict_to_df(inst_vel_diff_dict)
-    inst_vel_diff_all_binned_df=dict_to_df(inst_vel_diff_binned_dict)
-    inst_vel_ratio_df=dict_to_df(inst_vel_ratio_dict)
-    inst_vel_ratio_binned_df=dict_to_df(inst_vel_ratio_binned_dict)
-        
-    
-    
-    
-    
-    
-    # rad_dist_mean_all_df.to_csv("results\\rad_dist_mean_all{}_df.csv".format(time_thres),index=False) #writing the df to a csv file
-    # inst_vel_diff_all_df.to_csv("results\\inst_vel_diff_all_{}_df.csv".format(time_thres),index=False)
-    # inst_vel_diff_all_binned_df.to_csv("results\\inst_vel_diff_all_binned_{}_df.csv".format(time_thres),index=False)
-    
+                inst_vel_behav=np.append(inst_vel_before,inst_vel_after)
+                
+                inst_vel_after_binned=binning(inst_vel_after,10)
+                inst_vel_before_binned=binning(inst_vel_before,10)
+                
+                inst_vel_behav_binned=np.append(inst_vel_before_binned,inst_vel_after_binned)
+                
+                inst_vel_after_mean_all.append(np.nanmean(inst_vel_after))  #Calculate the mean Instantenous Velocity and append to a list                 
+                inst_vel_before_mean_all.append(np.nanmean(inst_vel_before))
+                inst_vel_diff_all.append(np.nanmean(inst_vel_after)-np.nanmean(inst_vel_before))
+                inst_vel_ratio_all.append(np.nanmean(inst_vel_after)/np.nanmean(inst_vel_before))
+                
+                inst_vel_after_binned_mean_all.append(np.nanmean(inst_vel_after_binned))  #Calculate the mean Instantenous Velocity and append to a list                 
+                inst_vel_before_binned_mean_all.append(np.nanmean(inst_vel_before_binned))
+                inst_vel_diff_binned_all.append(np.nanmean(inst_vel_after_binned)-np.nanmean(inst_vel_before_binned))
+                inst_vel_ratio_binned_all.append(np.nanmean(inst_vel_after_binned)/np.nanmean(inst_vel_before_binned))
+                
+                inst_vel_behav_dict[index]=inst_vel_behav
+                inst_vel_behav_df=pd.DataFrame(dict([ (k,pd.Series(v)) for k,v in inst_vel_behav_dict.items() ]))    
 
     
-    """
-    Here we generate comparative boxplots for genotypes, but mean only
-    """
+    individual_plot(inst_vel_behav_df)
+                
     
-    custom_plot(inst_vel_ratio_binned_df)
-    # fig.savefig('results\\inst_vel_{}seconds_means.png'.format(time_thres),format='png', dpi=600, bbox_inches = 'tight')
+    number_of_flies[genotype]=index
+    
+    
+    
+    inst_vel_after_mean_all_dict[genotype]=inst_vel_after_mean_all #Append the Mean Instantaneous Velocity LIST to Dictionary with the Corresponding Genotype
+    inst_vel_before_mean_all_dict[genotype]=inst_vel_before_mean_all
+    inst_vel_diff_dict[genotype]=inst_vel_diff_all
+    inst_vel_ratio_dict[genotype]=inst_vel_ratio_all
+    
+    inst_vel_after_binned_mean_all_dict[genotype]=inst_vel_after_binned_mean_all #Append the Mean Instantaneous Velocity LIST to Dictionary with the Corresponding Genotype
+    inst_vel_before_binned_mean_all_dict[genotype]=inst_vel_before_binned_mean_all
+    inst_vel_diff_binned_dict[genotype]=inst_vel_diff_binned_all
+    inst_vel_ratio_binned_dict[genotype]=inst_vel_ratio_binned_all
+    
+"""
+Converting Dictionaries to Dataframe using functions
+
+"""
+inst_vel_after_mean_all_df = dict_to_df(inst_vel_after_mean_all_dict)
+inst_vel_after_binned_mean_all_df= dict_to_df(inst_vel_after_binned_mean_all_dict)
+inst_vel_before_mean_all_df = dict_to_df(inst_vel_before_mean_all_dict)
+inst_vel_before_binned_mean_all_df = dict_to_df(inst_vel_before_binned_mean_all_dict)
+inst_vel_diff_all_df=dict_to_df(inst_vel_diff_dict)
+inst_vel_diff_all_binned_df=dict_to_df(inst_vel_diff_binned_dict)
+inst_vel_ratio_df=dict_to_df(inst_vel_ratio_dict)
+inst_vel_ratio_binned_df=dict_to_df(inst_vel_ratio_binned_dict)
+    
+
+
+
+
+
+# rad_dist_mean_all_df.to_csv("results\\rad_dist_mean_all{}_df.csv".format(time_thres),index=False) #writing the df to a csv file
+# inst_vel_diff_all_df.to_csv("results\\inst_vel_diff_all_{}_df.csv".format(time_thres),index=False)
+# inst_vel_diff_all_binned_df.to_csv("results\\inst_vel_diff_all_binned_{}_df.csv".format(time_thres),index=False)
+
+
+
+"""
+Here we generate comparative boxplots for genotypes, but mean only
+"""
+
+# custom_plot(inst_vel_ratio_binned_df)
+# fig.savefig('results\\inst_vel_{}seconds_means.png'.format(time_thres),format='png', dpi=600, bbox_inches = 'tight')
     
 # fig, ax= plt.subplots()
 # ax.plot(np.arange(0,time_thres*24,1), inst_vel_after)
