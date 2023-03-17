640db015b2fb0ec5664a54a78d1edf6ab53f45a1
namanagrawal97 <na488@cornell.edu>
2023-03-16 22:58:10 -0400

    ADDED PLOT ELEMENTS

--- DAM_experiments.py
+++ DAM_experiments.py
@@ -18,12 +18,11 @@ import glob
 import os
 import math
 import scipy.stats as stats
-import dabest
 from statsmodels.graphics.gofplots import qqplot
 from scipy.stats import mannwhitneyu
 from matplotlib.patches import Rectangle
 
-os.chdir('E:\\Dropbox\\DAM_experiments')
+os.chdir('C:\\Users\\sinha\\Dropbox\\DAM_experiments')
 
 df1=pd.read_csv('Monitor1-w1118xUAS-TNT.csv')
 df2=pd.read_csv('Monitor2-w1118.csv')
@@ -42,9 +41,9 @@ ZT_trimmed=df1.drop(df1.index[0:something])
 ZT_trimmed=ZT_trimmed.drop(df1.index[27556:27559])
 
 ZT_trimmed.shape
-allah=ZT_trimmed.iloc[:,10:]
+allah=ZT_trimmed.iloc[:,16:]
 allah.shape[1]
-allah.columns=np.arange(1,33,1)
+allah.columns=np.arange(1,allah.shape[1]+1,1)
 allah=allah.reset_index(drop=True)
 
 summed_all_flies=allah.sum(axis='columns')    
@@ -60,9 +59,9 @@ ZT_trimmed_w1118=df2.drop(df2.index[0:something])
 ZT_trimmed_w1118=ZT_trimmed_w1118.reset_index(drop=True)
 ZT_trimmed_w1118=ZT_trimmed_w1118.drop(df2.index[0:4])
 
-allah_w1118=ZT_trimmed_w1118.iloc[:,10:]
+allah_w1118=ZT_trimmed_w1118.iloc[:,16:]
 allah_w1118.shape[1]
-allah_w1118.columns=np.arange(1,33,1)
+allah_w1118.columns=np.arange(1,allah_w1118.shape[1]+1,1)
 allah_w1118=allah_w1118.reset_index(drop=True)
 allah_w1118=allah_w1118.dropna(axis=0,how='any')
 summed_all_flies_w1118=allah_w1118.sum(axis='columns')    
@@ -79,9 +78,9 @@ ZT_trimmed_uastnt=ZT_trimmed_uastnt.reset_index(drop=True)
 ZT_trimmed_uastnt=ZT_trimmed_uastnt.drop(df3.index[0:4])
 
 ZT_trimmed_uastnt.shape
-allah_uastnt=ZT_trimmed_uastnt.iloc[:,10:]
+allah_uastnt=ZT_trimmed_uastnt.iloc[:,16:]
 allah_uastnt.shape[1]
-allah_uastnt.columns=np.arange(1,33,1)
+allah_uastnt.columns=np.arange(1,allah_uastnt.shape[1]+1,1)
 allah_uastnt=allah_uastnt.reset_index(drop=True)
 
 summed_all_flies_uastnt=allah_uastnt.sum(axis='columns')    
@@ -99,13 +98,18 @@ ax.plot(np.arange(0,len(sums_uastnt),1), sums_uastnt,label=legend_lines[2])
 locs=ax.get_xticks()
 ax.legend(loc='upper right', prop={'size': 6})
 ax.set_xticks(np.arange(3,len(sums)-1,24))
-ax.set_xticklabels(np.arange(0,len(sums),24))
-ax.grid(True)
-# ax.add_patch(Rectangle((0, 0), 24, np.max(sums)+10,alpha=0.1,color='y'))
-# ax.add_patch(Rectangle((24, 0), 24, np.max(sums)+10,alpha=0.1,color='k'))
-# ax.add_patch(Rectangle((48, 0), 24, np.max(sums)+10,alpha=0.1,color='y'))
-# ax.add_patch(Rectangle((72, 0), 24, np.max(sums)+10,alpha=0.1,color='k'))
-# ax.add_patch(Rectangle((96, 0), 24, np.max(sums)+10,alpha=0.1,color='y'))
-# ax.add_patch(Rectangle((120, 0), 24, np.max(sums)+10,alpha=0.1,color='k'))
-# ON=ax.add_patch(Rectangle((144, 0), 24, np.max(sums)+10,alpha=0.1,color='y'))
-# OFF=ax.add_patch(Rectangle((168, 0), 24, np.max(sums)+10,alpha=0.1,color='k'))
+ax.set_xticklabels(np.arange(0,len(sums)/2,12))
+# ax.grid(True)
+j=2
+for i in range(3,len(sums),24):
+    if j%2==0:
+        ax.add_patch(Rectangle((i, 0), 24, np.max(sums)+10,alpha=0.1,color='y'))
+    else:
+        ax.add_patch(Rectangle((i, 0), 24, np.max(sums)+10,alpha=0.1,color='k'))
+    j=j+1
+ax.text(7, np.max(sums)-100, 'LIGHTS ON', fontsize=6)
+ax.text(33, np.max(sums)-100, 'LIGHTS OFF', fontsize=6)
+
+ax.set_xlabel('Hours')
+ax.set_ylabel('Sum of Activity of 26 flies')
+ax.set_title('Activity comparison of control genotypes in the DAM system')
